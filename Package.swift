0d852f4fae25746b7a2efcc150e6e5361e101b68
Magnus Jensen <mkj@vertica.dk>
2021-08-24 21:06:12 +0200

    use url-image 3.0.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "URLImage", url: "https://github.com/dmytro-anokhin/url-image", .branch("main"))
+        .package(name: "URLImage", url: "https://github.com/dmytro-anokhin/url-image",
+                 .upToNextMajor(from: .init(3, 0, 0)))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
